a046c2c8edce775a1dc2a7084e5aa24d7a134f4d
nilesh-30 <nileshk7017@gmail.com>
2025-09-09 17:19:15 +0530

    added search functionality

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 const container = document.querySelector('.container');
+const input = document.querySelector('input');
 
-fetch("https://pbivizedit.com/api/visuals")
-.then((res) => res.json())
-.then((data) => {
-  data.items.forEach((item) => {
-    const galleryItem = document.createElement('div');
-    galleryItem.classList.add('items');
-    galleryItem.innerHTML = `
-      <a title=${item.description} href="#">
-        <img src=${item.imagePath} alt=${item.name}>
-        <hr>
-        <div class="chartName">${item.name}</div>
-      </a>
-    `;
-    container.append(galleryItem);
-  })
+function renderComponent(item) {
+  const galleryItem = document.createElement('div');
+  galleryItem.classList.add('items');
+  galleryItem.innerHTML = `
+    <a title=${item.description} href="#">
+      <img src=${item.imagePath} alt=${item.name}>
+      <hr>
+      <div class="chartName">${item.name}</div>
+    </a>
+  `;
+  container.append(galleryItem);
+}
+
+function fetchDataAndRender() {
+  fetch("https://pbivizedit.com/api/visuals")
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log("starting", data)
+      data.items.forEach((item) => {
+        renderComponent(item);
+      })
+    })
+}
+
+fetchDataAndRender()
+
+input.addEventListener('input', (e) => {
+  const value = (e.target.value).toLowerCase();
+  search(value);
+  console.log(value, typeof value)
 })
+
+function search(value) {
+  if (value === "") {
+    fetchDataAndRender();
+  }
+  else {
+    fetch("https://pbivizedit.com/api/visuals")
+      .then(res => res.json())
+      .then((data) => {
+        const charts = data.items.filter(item => item.id.includes(value));
+        // console.log(charts)
+        container.innerHTML = "";
+        charts.forEach((item) => {
+          renderComponent(item)
+        })
+      })
+  }
+}
+
